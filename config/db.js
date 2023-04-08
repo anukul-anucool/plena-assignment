dbf287ef286a8e248d527fbf071acee6e4c7b497
Anukul kumar <44088496+anukul-anucool@users.noreply.github.com>
2023-04-08 08:53:30 +0530

    restructure

---
+++ config/db.js
@@ -0,0 +1,8 @@
+module.exports = {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || '5432',
+    database: process.env.DB_NAME || 'mydb',
+    username: process.env.DB_USERNAME || 'username',
+    password: process.env.DB_PASSWORD || 'password',
+  };
+  
